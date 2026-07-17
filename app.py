1013a28a4d8c27396d645aad4709ef12d5b1d6eb
PARTH_DS <parthvashishtha498@gmail.com>
2026-07-17 11:54:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ interface = gr.Interface(
     ],
     outputs=gr.Text(label="Assessment Result"),
     title="Diabetes Prediction System",
-    description="Enter the medical metrics to predict diabetes risk using a Decision Tree Machine Learning model."
+    description="Project by Parth Enter the medical metrics to predict diabetes risk using a Decision Tree Machine Learning model."
 )
 # ------------------------------------------
 
